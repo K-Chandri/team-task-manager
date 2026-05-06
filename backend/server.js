9ea4d2a591c8c5e85f8a2613de9295edf2bf1052
Chandrika <chandrikakunchala09@gmail.com>
2026-05-06 21:52:59 +0530

    fixed railway env issue

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Server Running");
+});
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT || 5000, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
 })
 .catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-    res.send("Server Running");
-});
