e5d5b58b3651e5800ba3247824d6e40f08918b63
Chandrika <chandrikakunchala09@gmail.com>
2026-05-06 21:35:41 +0530

    final mongodb fix

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/projects', require('./routes/projects'));
-app.use('/api/tasks', require('./routes/tasks'));
-
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log('MongoDB Connected!');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
     });
-  })
-  .catch(err => console.log(err));
+})
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+});
